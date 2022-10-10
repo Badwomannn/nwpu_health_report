e6bf02f82061c1521f58f4ebbdb67dbbbfafd79e
Louis Wong <60389209+Badwomannn@users.noreply.github.com>
2022-10-10 11:10:41 +0800

    Update health_report.py

--- health_report.py
+++ health_report.py
@@ -31,8 +31,8 @@ password.send_keys(PASSWORD)
 browser.find_element(By.CSS_SELECTOR, "#fm1 > div:nth-child(4) > div > input.el-button.el-button--primary.el-button--medium.is-round").click()
 time.sleep(1)
 
-def page_is_loaded(driver):
-    return driver.find_element_by_tag_name("body") != None
+def page_is_loaded(browser):
+    return browser.find_element_by_tag_name("body") != None
 
 browser.get('https://yqtb.nwpu.edu.cn/wx/ry/jrsb_xs.jsp')
 wait = ui.WebDriverWait(browser, 10)
