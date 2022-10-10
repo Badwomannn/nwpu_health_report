4005a2021009599a99b8ccb8c3a6dafd02cc54b5
Louis Wong <60389209+Badwomannn@users.noreply.github.com>
2022-10-10 11:08:18 +0800

    Update health_report.py

--- health_report.py
+++ health_report.py
@@ -1,6 +1,7 @@
 import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import ui
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.action_chains import ActionChains
